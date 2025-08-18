3dfc2d9912d03091201c22a154ce9e31860b6c00
Sulfurbottom <397love@gmail.com>
2025-08-18 13:33:34 +0900

    lottie-react 패키지를 추가하고, 관련 의존성을 pnpm-lock.yaml에 업데이트했습니다. ThirdSection 컴포넌트에서 Lottie 애니메이션을 적용하여 사용자 인터페이스를 개선했습니다. 또한, VSCode 설정에서 파일 감시 제외 항목을 추가했습니다.

--- app/(root)/introduction/section/third.tsx
+++ app/(root)/introduction/section/third.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import Third1 from "@/app/asset/third_1.svg";
 import Third2 from "@/app/asset/third_2.svg";
 import Third3 from "@/app/asset/third_3.svg";
 import ThirdLogo from "@/app/asset/third_logo.svg";
+import Lottie from "lottie-react";
+import animationData from "@/app/asset/lottie/Contents 02_test.json";
 
 export default function ThirdSection() {
   return (
     <div className="w-full h-fit bg-black mt-60 py-60 rounded-t-[60px] flex flex-col items-center">
       <Third1 />
-      <ThirdLogo />
+      <Lottie animationData={animationData} loop={true} />
+      {/* <ThirdLogo /> */}
       <Third2 />
       <Third3 />
     </div>
